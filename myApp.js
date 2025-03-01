349b5c8bfb9fe081989e4833701d3f719f11a801
Ching-fung WAN <cfwan6@gmail.com>
2025-03-01 23:46:46 +0000

    fix bug

--- myApp.js
+++ myApp.js
@@ -12,11 +12,15 @@ app.get("/", function(req, res){
 app.use("/public", express.static(__dirname + '/public'));
 
 app.get("/json", function(req, res){
+    let response;
+
     if (process.env.MESSAGE_STYLE === "uppercase"){
         response = "Hello json".toUpperCase();
     } else {
         response = "Hello json";
     }
+
+    res.json({ "message": response})
 })
 
 
