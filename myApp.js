51ea5d833ae8a9b109eb61255a8972eae262ef6f
Ching-fung WAN <cfwan6@gmail.com>
2025-03-01 23:35:31 +0000

    add env file and change message into uppercase

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 let express = require('express');
 const res = require('express/lib/response');
 let app = express();
@@ -10,6 +12,10 @@ app.get("/", function(req, res){
 app.use("/public", express.static(__dirname + '/public'));
 
 app.get("/json", function(req, res){
+    let message = "Hello json"
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        message = message.toUpperCase();
+    }
     res.json({"message": "Hello json"});
 })
 
