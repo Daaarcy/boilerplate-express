d2888d8b40961a68204db5477fb7d9656bbfb9f2
Ching-fung WAN <cfwan6@gmail.com>
2025-03-01 22:17:27 +0000

    fix typo

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ app.get("/", function(req, res){
 
 app.use("/public", express.static(__dirname + '/public'));
 
-app.get("/json", funtion(req, res){
+app.get("/json", function(req, res){
     res.json({"message": "Hello json"});
 })
 
