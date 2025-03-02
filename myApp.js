88ebb6bb174bdc5a667073b9fc3aa342e17e22be
Ching-fung WAN <cfwan6@gmail.com>
2025-03-02 00:05:40 +0000

    edited code

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
+require('dotenv').config();
 
 let express = require('express');
 const res = require('express/lib/response');
 let app = express();
-absolutePath = __dirname + '/views/index.html'
+let absolutePath = __dirname + '/views/index.html'
 
 app.get("/", function(req, res){
     res.sendFile(absolutePath);
